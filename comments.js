eff9d27864b008d6e815109f8911fffe20819887
DamianM <damian.michalski@kramp.com>
2019-02-12 10:37:10 +0100

    Poprawa comments.js

--- comments.js
+++ comments.js
@@ -4,39 +4,43 @@ import {EDIT_COMMENT} from './actions';
 import {THUMB_UP_COMMENT} from './actions';
 import {THUMB_DOWN_COMMENT} from './actions';
 
+
 function comments(state = [], action) {
-    switch(action.type) {
-        case ADD_COMMENT:
-            return [{
-                id: action.id,
-                text: action.text,
-                votes: 0
-            }
-            , ...state.comments];
-        case REMOVE_COMMENT:
-            return [{
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            }];
-        case EDIT_COMMENT:
-            return [{
-                    id: action.id,
-                    text: action.text,
-                    votes: state.comments.find(comment => comment.id === action.id).votes
-                }
-                , ...state.comments];
-        case THUMB_UP_COMMENT:
-            return [{
-                    id: action.id,
-                    text: state.comments.find(comment => comment.id === action.id).text,
-                    votes: action.amount++
-                }
-                , ...state.comments];
-        case THUMB_DOWN_COMMENT:
-            return [{
-                    id: action.id,
-                    text: state.comments.find(comment => comment.id === action.id).text,
-                    votes: action.amount--
-                }
-                , ...state.comments];
+
+  function modifyThumbs(state, toAdd) {
+    return state.map(comment => {
+      if (comment.id === action.id) {
+      return {...comment, votes: comment.votes + toAdd}
     }
+    return comment;
+  });
+  }
+
+  switch (action.type) {
+    case ADD_COMMENT:
+      return [{
+        id: action.id,
+        text: action.text,
+        votes: 0
+      }
+        , ...state];
+
+    case REMOVE_COMMENT:
+      return state.filter(comment => comment.id !== action.id);
+    case EDIT_COMMENT:
+      return state.map(comment => {
+        if (comment.id === action.id) {
+      comment.text = action.text;
+    }
+      return comment;
+  });
+case THUMB_UP_COMMENT:
+      return modifyThumbs(state, +1);
+case THUMB_DOWN_COMMENT:
+      return modifyThumbs(state, -1);
+default:
+  return state;
 }
+}
+
+export default comments;
