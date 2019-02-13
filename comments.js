a8b955431e02d95bbd04ead257831e342d615700
DamianM <damian.michalski@kramp.com>
2019-02-13 10:32:27 +0100

    Formatowanie

--- comments.js
+++ comments.js
@@ -5,42 +5,41 @@ import { THUMB_UP_COMMENT } from "./actions";
 import { THUMB_DOWN_COMMENT } from "./actions";
 
 function comments(state = [], action) {
-  function modifyThumbs(state, toAdd) {
-    return state.map(comment => {
-      if (comment.id === action.id) {
-      return { ...comment, votes: comment.votes + toAdd };
+    function modifyThumbs(state, toAdd) {
+        return state.map(comment => {
+            if (comment.id === action.id) {
+                return { ...comment, votes: comment.votes + toAdd };
+            }
+            return comment;
+        });
     }
-    return comment;
-  });
-  }
 
-  switch (action.type) {
-    case ADD_COMMENT:
-      return [
-        {
-          id: action.id,
-          text: action.text,
-          votes: 0
-        },
-          ...state
-    ];
+    switch (action.type) {
+        case ADD_COMMENT:
+            return [{
+                    id: action.id,
+                    text: action.text,
+                    votes: 0
+                },
+                ...state
+            ];
 
-    case REMOVE_COMMENT:
-      return state.filter(comment => comment.id !== action.id);
-    case EDIT_COMMENT:
-      return state.map(comment => {
-        if (comment.id === action.id) {
-      comment.text = action.text;
+        case REMOVE_COMMENT:
+            return state.filter(comment => comment.id !== action.id);
+        case EDIT_COMMENT:
+            return state.map(comment => {
+                if (comment.id === action.id) {
+                    comment.text = action.text;
+                }
+                return comment;
+            });
+        case THUMB_UP_COMMENT:
+            return modifyThumbs(state, +1);
+        case THUMB_DOWN_COMMENT:
+            return modifyThumbs(state, -1);
+        default:
+            return state;
     }
-      return comment;
-  });
-case THUMB_UP_COMMENT:
-      return modifyThumbs(state, +1);
-case THUMB_DOWN_COMMENT:
-      return modifyThumbs(state, -1);
-default:
-  return state;
-}
 }
 
 export default comments;
